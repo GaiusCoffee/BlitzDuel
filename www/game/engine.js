6dab2b9d110d74c0734aa67211ab4a7554eb163b
Erik Gaius Capistrano <gaius.coffee@gmail.com>
2017-05-06 03:09:37 +0800

    Fix typos.

--- www/game/engine.js
+++ www/game/engine.js
@@ -5,7 +5,7 @@ class engine{
         this.g.canvasW = ((9/16) * window.innerHeight) * window.devicePixelRatio;
         this.p = new Phaser.Game(this.g.canvasW, this.g.canvasH, Phaser.CANVAS, 'game');
         this.p.state.add("title", new screenTitle(this));
-        this.p.state.add("gane", new screenGame(this));
+        this.p.state.add("game", new screenGame(this));
         this.p.state.start("title");
     }
     pause(){
